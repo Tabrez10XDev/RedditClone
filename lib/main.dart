3150f99871773c02973fdb84551ff8bb7e65c915
lowjunkie <meet.tabrezt1@gmail.com>
2024-01-25 04:44:10 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/models/user_model.dart';
+import 'package:reddit_clone/router.dart';
+import 'package:reddit_clone/theme/palette.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:routemaster/routemaster.dart';
+import 'core/common/error_text.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref.watch(authControllerProvider.notifier).getUserData(data.uid).first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ref.watch(authStateChangeProvider).when(
+        data: (data) => MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: Palette.darkModeAppTheme,
+              routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+                if (data != null) {
+                  getData(ref, data);
+                  if(userModel != null){
+                    return loggedInRoute;
+                  }else{
+                    return loggedOutRoute;
+                  }
+                } else {
+                  return loggedOutRoute;
+                }
+              }),
+              routeInformationParser: RoutemasterParser(),
+            ),
+        error: (error, stackTrace) => ErrorText(error: error.toString()),
+        loading: () => Loader());
+  }
+}
