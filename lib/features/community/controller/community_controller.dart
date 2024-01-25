47e22c9ccddcf6da0342f39165c3284310bee47e
lowjunkie <meet.tabrezt1@gmail.com>
2024-01-26 03:16:29 +0530

    Working on Logout

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/providers/storage_repository_provider.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/community/repository/community_repository.dart';
 import 'package:reddit_clone/models/community_model.dart';
@@ -8,31 +11,46 @@ import 'package:routemaster/routemaster.dart';
 import '../../../core/constants/constants.dart';
 import '../../../core/utils.dart';
 
-final userCommunitiesProvider = StreamProvider((ref){
+final userCommunitiesProvider = StreamProvider((ref) {
   final communityController = ref.watch(communityControllerProvider.notifier);
   return communityController.getUserCommunities();
 });
 
 final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
-  return ref.watch(communityControllerProvider.notifier).getCommunityByName(name);
+  return ref
+      .watch(communityControllerProvider.notifier)
+      .getCommunityByName(name);
+});
+
+final searchCommunityProvider = StreamProvider.family((ref, String query){
+  return ref
+      .watch(communityControllerProvider.notifier)
+      .searchCommunity(query);
 });
 
 final communityControllerProvider =
     StateNotifierProvider<CommunityController, bool>((ref) {
   final communityRepository = ref.watch(communityRepositoryProvider);
+  final storageRepository = ref.watch(storageRepositoryProvider);
   return CommunityController(
-      communityRepository: communityRepository, ref: ref,
-  );
+      communityRepository: communityRepository,
+      ref: ref,
+      storageRepository: storageRepository);
 });
+
 
 class CommunityController extends StateNotifier<bool> {
   final CommunityRepository _communityRepository;
   final Ref _ref;
+  final StorageRepository _storageRepository;
 
   CommunityController(
-      {required CommunityRepository communityRepository, required Ref ref})
+      {required CommunityRepository communityRepository,
+      required Ref ref,
+      required StorageRepository storageRepository})
       : _communityRepository = communityRepository,
         _ref = ref,
+        _storageRepository = storageRepository,
         super(false);
 
   void createCommunity(String name, BuildContext context) async {
@@ -55,13 +73,55 @@ class CommunityController extends StateNotifier<bool> {
     });
   }
 
-  Stream<List<Community>> getUserCommunities(){
+  Stream<List<Community>> getUserCommunities() {
     final uid = _ref.read(userProvider)!.uid;
     return _communityRepository.getUserCommunities(uid);
   }
 
-  Stream<Community> getCommunityByName(String name){
+  Stream<Community> getCommunityByName(String name) {
     return _communityRepository.getCommunityByName(name);
   }
+
+  void editCommunity({
+    required File? profileFile,
+    required Community community,
+    required File? bannerFile,
+    required BuildContext context,
+  }) async {
+
+    state = true;
+
+    if (profileFile != null) {
+      final res = await _storageRepository.storeFile(
+          path: 'communities/profile', id: community.name, file: profileFile);
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => community = community.copyWith(avatar: r),
+      );
+    }
+
+    if (bannerFile != null) {
+      final res = await _storageRepository.storeFile(
+          path: 'communities/banner', id: community.name, file: bannerFile);
+      res.fold(
+            (l) => showSnackBar(context, l.message),
+            (r) => community = community.copyWith(banner: r),
+      );
+    }
+
+    final res = await _communityRepository.editCommunity(community);
+
+    state = false;
+
+    res.fold(
+            (l) => showSnackBar(context, l.message),
+            (r) => Routemaster.of(context).pop(),
+    );
+
+  }
+
+Stream<List<Community>> searchCommunity(String query){
+    return _communityRepository.searchCommunity(query);
+}
 
 }
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/home/delegates/search_community_delegate.dart';
 import 'package:reddit_clone/features/home/drawers/community_list_drawer.dart';
+import 'package:reddit_clone/features/home/drawers/profile_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   void displayDrawer(BuildContext context){
     Scaffold.of(context).openDrawer();
+  }
+
+  void displayEndDrawer(BuildContext context){
+    Scaffold.of(context).openEndDrawer();
   }
 
   @override
@@ -27,14 +33,20 @@ class HomeScreen extends ConsumerWidget {
           }
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: CircleAvatar(
-            radius: 16,
-            backgroundImage: NetworkImage(user!.profilePic),
-          ))
+          IconButton(onPressed: (){
+            showSearch(context: context, delegate: SearchCommunityDelegate(ref));
+          }, icon: const Icon(Icons.search)),
+          GestureDetector(
+            onTap: () => displayEndDrawer(context),
+            child: IconButton(onPressed: (){}, icon: CircleAvatar(
+              radius: 16,
+              backgroundImage: NetworkImage(user!.profilePic),
+            )),
+          )
         ],
       ),
-      drawer: CommunityListDrawer(),
+      drawer: const CommunityListDrawer(),
+      endDrawer: const ProfileDrawer(),
     );
   }
 }
