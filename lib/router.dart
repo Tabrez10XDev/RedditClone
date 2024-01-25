c3bd2d8700aec0d34e3f95c0cd26aa5b100da4f5
lowjunkie <meet.tabrezt1@gmail.com>
2024-01-25 20:03:05 +0530

    Working on Create Community screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:reddit_clone/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -8,5 +9,6 @@ final loggedOutRoute = RouteMap(routes: {
 });
 
 final loggedInRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen())
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen())
 });
